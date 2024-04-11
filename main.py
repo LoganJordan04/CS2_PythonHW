dfa7ecc9f25380f1b39cdd9478385d9e9543d277
Logan <ljordan@cocc.edu>
2024-04-11 11:23:25 -0700

    Added pie chart

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 import matplotlib.pyplot as plot
 
-# set up your lists
-numlist = [8, 6, 5, 3]
-namelist = ['freshmen', 'sophomores', 'juniors', 'seniors']
-colorlist = ['red', 'green', 'pink', 'yellow']
-explodelist = [0.1, 0.0, 0.0, 0.0]
+# Set up the lists.
+labels = ["Java", "Python", "JavaScript", "C/C++", "SQL", "PHP", "C#"]
+sizes = [22, 21, 14, 10, 6, 5, 3]
+colors = ["#ed2b2d", "#4987b7", "#f2dc56", "#0c4a86", "#dd793b", "#7b7fb6", "#a177da"]
 
-# make the pie chart
-plot.pie(numlist, labels=namelist, autopct='%.2f%%', colors=colorlist, explode=explodelist, startangle=90)
-plot.axis('equal')
-plot.savefig('piechart.png')
+# Make the pie chart.
+fig1, ax1 = plot.subplots()
+patches, texts, autotexts = ax1.pie(sizes, colors=colors, labels=labels, autopct='%1.1f%%', startangle=90,
+                                    explode=[0, 0.1, 0, 0, 0, 0, 0])
+
+# Display the pie chart.
+ax1.set(aspect="equal", title="Most Used Programming Languages\nLJ")
+plot.tight_layout()
+plot.show()
