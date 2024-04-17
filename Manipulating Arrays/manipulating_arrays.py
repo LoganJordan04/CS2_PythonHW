1b2fd791c4090eab8109a4e86489bfac025a5042
Logan <ljordan@cocc.edu>
2024-04-16 18:04:13 -0700

    Manipulating Arrays done

--- Manipulating Arrays/manipulating_arrays.py
+++ Manipulating Arrays/manipulating_arrays.py
@@ -7,5 +7,5 @@ if __name__ == '__main__':
     print(f"Entire array:\n{array}")
     print(f"\nNumber from the 2nd row, 3rd column: {array[1, 2]}")
     print(f"\nSum of all array elements: {np.sum(array)}")
-    print(f"\nMean of each row in the array: {np.mean(array, axis=1)}")
+    print(f"\nMean of each row: {np.mean(array, axis=1)}")
     print(f"\nMaximum value in each column: {np.max(array, axis=0)}")
