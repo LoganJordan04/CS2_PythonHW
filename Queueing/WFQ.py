72acb449f46265e8e8ee4ac414bd2b4a8edd7186
Logan <ljordan@cocc.edu>
2024-04-18 12:18:17 -0700

    Queues from input file

--- Queueing/WFQ.py
+++ Queueing/WFQ.py
@@ -1,1 +1,29 @@
-# Queueing in python assignment
+def main():
+    # Reading the input_queues file.
+    input_file = open("input_queue.txt", "r")
+    input_data = input_file.read()
+    input_file.close()
+
+    # Input packets as string list.
+    input_packets = input_data.split("\n")
+
+    # Queues from input packets, P=Priority, S=Standard, E=Economy.
+    p_queue = []
+    s_queue = []
+    e_queue = []
+
+    for packet in input_packets:
+        if packet[0] == "P":
+            p_queue.append(packet)
+        elif packet[0] == "S":
+            s_queue.append(packet)
+        elif packet[0] == "E":
+            e_queue.append(packet)
+        else:
+            print(f"'{packet}' is an abnormal packet!")
+
+    print(f"P: {p_queue}\nS: {s_queue}\nE: {e_queue}")
+
+
+if __name__ == '__main__':
+    main()
